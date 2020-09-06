68e1e6ed81cd9633bafc8e645bd39ff6fbc67c6b
KyleTruong <ktruong008@gmail.com>
2020-09-06 15:14:10 -0400

    [feature] Add hasura

---
+++ hasura/migrations/1599408206601_create_table_public_shop/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."shop"("shop_origin" text NOT NULL, PRIMARY KEY ("shop_origin") );
--- server.js
+++ server.js
@@ -10,18 +10,28 @@ const APP_NAME = process.env.APP_NAME || 'poc';
 
 app.use(async (ctx) => {
   const path = ctx?.request?.url;
+
+  /**
+   * Generic test
+   */
   if (path === '/') {
     console.log('main');
     ctx.body = `Hello world, from ${APP_NAME}`;
     return;
   }
 
+  /**
+   * K8s health check
+   */
   if (path === '/health') {
     console.log('health');
     ctx.body = `Healthy, from ${APP_NAME}`;
     return;
   }
 
+  /**
+   * Route to test microservices talking to each other
+   */
   if (path === '/special') {
     console.log('special');
     ctx.body = {
@@ -30,6 +40,9 @@ app.use(async (ctx) => {
     return;
   }
 
+  /**
+   * Making a request to the other service via K8s DNS
+   */
   if (path === '/fetch') {
     // service.namespace.svc.cluster.local
     const serviceName = process.env.SERVICE_NAME;
@@ -44,6 +57,45 @@ app.use(async (ctx) => {
     ctx.body = {
       data: response,
     };
+    return;
+  }
+
+  /**
+   * Hasura
+   */
+  async function hasuraGraphqlRequest(query, variables = {}) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': 'password',
+    }
+
+    const body = JSON.stringify({
+      query,
+      variables: variables || null,
+    });
+
+    const url = `http://${process.env.HASURA_BASE_URL}/v1/graphql`
+
+    return await fetch(url, {
+      method: 'POST',
+      headers,
+      body,
+    })
+      .then(stream => stream.json())
+  }
+
+  if (path === '/shops') {
+    const response = await hasuraGraphqlRequest(
+      `
+      query MyQuery {
+        shop {
+          shop_origin
+        }
+      }
+      `
+    )
+
+    ctx.body = response
     return;
   }
 });
